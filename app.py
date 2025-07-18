3dd16eaf2520f8dccccef76716b2de0ad956ff8a
Lahdhiri Moetez <moetez.lahdhiri@student-cs.fr>
2025-07-18 20:24:15 +0200

    enable public link

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import gradio as gr
 def greet(name):
     return "Hello " + name + "!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo = gr.Interface(fn=greet, inputs="text", outputs="text").launch(share=True)
 demo.launch()
